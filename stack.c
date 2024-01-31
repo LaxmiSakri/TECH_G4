69eeb9c385124aadf123deeae9402652f7782671
unknown <kaverishapeti07@gmail.com>
2024-01-31 10:13:39 +0530

    push opertion

---
+++ stack.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define size 5
+int top=-1,item,s[size];
+void push(){
+if(top==s[size]-1)
+{
+printf("stack overflow");
+}
+else{
+printf("element to be inserted");
+scanf("%d",&item);
+top++;
+s[top]=item
